1c72f402d6cc2c049a3375ff385356c104b8fa1f
Erin Chapman <erin@mcgladreychapman.com>
2022-01-22 06:56:31 -0800

    Online compiler I was using on my iPad didn't actually throw errors, cleanup time!

--- day_01/main.cpp
+++ day_01/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <string>
+
 #include "checkDepths.h"
 
 int main()
 {
   CheckDepths cp;
-  if(cp.readDepths("input") != CheckDepths::Result::SUCCESS)
+  if(cp.readDepths(std::string("input")) != CheckDepths::Result::SUCCESS)
     return -1;
   std::cout << "Measure to Measure: " << cp.countDeeper() << std::endl;
   std::cout << "Windows of 3: " << cp.countDeeperSlidingWindow(3) << std::endl;
